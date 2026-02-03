fff86956cc1ba09fd10167f6c311191a6f959236
Marco <madcarbone@gmail.com>
2026-02-03 02:11:26 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,29 @@
-const CACHE = "rider-tracker-v1.3";
+const CACHE = "rider-pwa-only-v1";
 const FILES = [
   "./",
   "./index.html",
+  "./app.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(FILES))
   );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
