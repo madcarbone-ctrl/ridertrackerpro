76a931b35a329f822af54a10c6d1610c5d891a7a
Marco <madcarbone@gmail.com>
2026-02-03 02:43:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,26 +9,17 @@ const FILES = [
   "./icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
 self.addEventListener("fetch", e => {
+  const req = e.request;
+
+  if (req.destination === "image") {
+    e.respondWith(
+      fetch(req).catch(() => caches.match(req))
+    );
+    return;
+  }
+
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(req).then(r => r || fetch(req))
   );
 });
